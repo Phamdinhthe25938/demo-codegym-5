755f293bde77f9f5cd92a8bab968c6c76ec612f9
unknown <phamdinhthe2509@gmail.com>
2022-04-09 14:23:48 +0700

    up demo bai7_2

---
+++ demo_bai7_2/demo7_2.js
@@ -0,0 +1,11 @@
+
+
+let inputHeight;
+let inputWitdh;
+inputHeight = prompt("nhap chieu dai :");
+inputWitdh = prompt("nhap chieu rong:");
+// ham chuyen sang kieu so nguyen :
+let witdh = parseInt(inputWitdh);
+let height = parseInt(inputHeight);
+let area = witdh*height;
+document.write("Dien tich la:"+area)
